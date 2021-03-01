d4554aebfd8d8b88ae93aff30ce4805028ed5773
sumeikom <sumeikom@gmail.com>
2021-03-01 01:35:02 -0500

    commit tree ac02caf70d69dc0233900160097b48ae100d178b

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,14 @@
 def using_while
+
   levitation_force = 6
-	
-  #your code here
+  # => 6 (return value)
+
+  while levitation_force < 10
+    puts "Wingardium Leviosa"
+    levitation_force = levitation_force + 1
+  end
 end
+
+# => nil (return value)
 
 
